1778a06bc7d1538340e9bea7cedf1d174dca72cf
skeczeli <skeczeli@gmail.com>
2025-04-01 15:06:18 -0300

    First commit: basic setup + login.

---
+++ src/main/java/com/example/demo/UserDTO.java
@@ -0,0 +1,50 @@
+package com.example.demo;
+
+public class UserDTO {
+    private String name;
+    private String username;
+    private String password;
+    private String email;
+
+    public UserDTO() {
+    }
+
+    public UserDTO(String username, String name, String password, String email) {
+        this.username = username;
+        this.name = name;
+        this.password = password;
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
