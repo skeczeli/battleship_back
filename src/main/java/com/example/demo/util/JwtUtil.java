b16497037d88344a7f546147d5b90bc8d3684d3c
skeczeli <skeczeli@gmail.com>
2025-04-08 10:00:50 -0300

    Jwt for proper authentication.

---
+++ src/main/java/com/example/demo/util/JwtUtil.java
@@ -0,0 +1,33 @@
+package com.example.demo.util;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    private final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+    private final long expirationMillis = 1000 * 60 * 60; // 1 hora
+
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + expirationMillis))
+                .signWith(key)
+                .compact();
+    }
+
+    public String validateTokenAndGetUsername(String token) {
+        try {
+            Jws<Claims> claims = Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+            return claims.getBody().getSubject();
+        } catch (JwtException e) {
+            return null; // token inválido
+        }
+    }
+}
