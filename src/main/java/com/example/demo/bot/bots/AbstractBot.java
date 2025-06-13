dc741665f99ec7da0c406ae8f88438c457cd25d9
Juan Luis Martino <jlmartino@mail.austral.edu.ar>
2025-06-12 23:05:26 -0300

    barco de 6 bot

--- src/main/java/com/example/demo/bot/bots/AbstractBot.java
+++ src/main/java/com/example/demo/bot/bots/AbstractBot.java
@@ -20,7 +20,7 @@ public abstract class AbstractBot implements BotStrategy {
         {1, 5}, {2, 4}, {3, 3}, {4, 3}, {5, 2}
     };
     protected static final int[][] SHIPS_14 = {
-        {1, 5}, {2, 5}, {3, 4}, {4, 3}, {5, 3}, {6, 3}, {7, 2}
+        {1, 5}, {2, 6}, {3, 4}, {4, 3}, {5, 3}, {6, 3}, {7, 2}
     };
 
     protected  final int[][] SHIPS;
