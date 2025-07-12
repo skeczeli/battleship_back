ac056c5395f218e32826801aaa33cede4ca42ab7
skeczeli <skeczeli@gmail.com>
2025-07-12 19:00:38 -0300

    Intento de debug para error de auth con la db

--- src/main/java/com/example/demo/PrintPasswordRunner.java
+++ src/main/java/com/example/demo/PrintPasswordRunner.java
@@ -1,18 +1,13 @@
 package com.example.demo;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
-
 
 @Component
 public class PrintPasswordRunner implements CommandLineRunner {
 
-    @Value("${spring.datasource.password}")
-    private String dbPassword;
-
     @Override
     public void run(String... args) {
-        System.out.println("🟣 DB PASSWORD (from env or properties): " + dbPassword);
+        System.out.println("✅ Backend arrancó y ejecutó código propio.");
     }
 }
